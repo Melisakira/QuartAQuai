0ba0025698ac01b18aa489ae4df73a8f413745fa
Melisakira <brandenbourgisabelle@outlook.com>
2026-08-11 12:31:44 +0200

    Code test -> Pattern (reçoit chaque incident)-> pas d'action menu, mais coordination déclenchée et escalade si gravité crtique

--- Equipage/OfficierDe Garde.cs
+++ Equipage/OfficierDe Garde.cs
@@ -1,18 +1,30 @@
-﻿using QuartAQuai.Alertes;
+﻿using System;
+using QuartDeMer.Alertes;
 
-namespace QuartAQuai.Equipage;
+namespace QuartDeMer.Equipage;
 
-public class Officier : MembreEquipage
+public class OfficierDeGarde : MembreEquipage
 {
-    public Officier(string nom, string grade, string posteAffecte)
+    public OfficierDeGarde(string nom, string grade, string posteAffecte)
         : base(nom, grade, posteAffecte)
     {
     }
 
     public override void ReagirAlerte(Incident incident)
     {
-        // Minimal
-        Console.WriteLine($"Officier {Nom} ({Grade}) répond à une alerte : {incident}");
+        Console.WriteLine($"{Nom} ({Grade}) — {PosteAffecte} : Je centralise l'information — {incident.Decrire()}");
+
+        if (incident.Gravite == "critique")
+        {
+            Console.WriteLine($"{Nom} ({Grade}) : Gravité critique, je sonne l'alarme générale, mobilise du renfort.");
+            EscaladerVersCommandant(incident);
+        }
+    }
+    public void EscaladerVersCommandant(Incident incident)
+        {
+            Console.WriteLine($"{Nom} ({Grade}) : Je réveille le commandant et l'en informe : {incident.Decrire()}");
+        }
     }
 
-}
+
+    
