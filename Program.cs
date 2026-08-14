e1dfe704d62c502fef9a9a06c5d0a44db1f5afa0
Melisakira <brandenbourgisabelle@outlook.com>
2026-08-14 21:20:16 +0200

    création des objets (navire, equipage), abonner

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
+using QuartAQuai.Alertes;
+using QuartAQuai.AQuai;
+using QuartAQuai.Equipage;
+
 namespace QuartAQuai;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Navire navire = new Navire("F911 Wesdiep", "Fr&gate");
+
+        Electricien electricien = new Electricien("Brandenbourg", "1er Maître", "Local technique");
+        Mecanicien mecanicien = new Mecanicien("Lefevre", "Quartier-Maître", "Salle des machines");
+        OfficierDeGarde officier = new OfficierDeGarde("Dupont", "Enseigne de vaisseau", "Poste de garde");
+        RondeurSecurite rondeur = new RondeurSecurite("Dubois", "Matelot", "Aucun poste fixe - rondes");
+        VeilleurCoupee veilleur = new VeilleurCoupee("Marchand", "1er Matelot", "Coupée");
+
+        CentreAlerte centreAlerte = new CentreAlerte();
+        centreAlerte.Abonner(electricien);
+        centreAlerte.Abonner(mecanicien);
+        centreAlerte.Abonner(officier);
+        centreAlerte.Abonner(rondeur);
+        centreAlerte.Abonner(veilleur);
+
+        JournalDeBord journal = new JournalDeBord();
+
         bool continuerLeQuart = true;
         while (continuerLeQuart)
         {
@@ -19,11 +40,11 @@ class Program
                 case "1":
                     Console.WriteLine("(à coder)"); break;
                 case "2":
-                    FaireRonde(); break;
+                    FaireRonde(navire, rondeur, journal, centreAlerte); break;
                 case "3":
-                    AssurerVeille(); break;
+                    AssurerVeille(navire, veilleur, journal, centreAlerte); break;
                 case "4":
-                    DeclarerIncident(); break;
+                    DeclarerIncident(journal, centreAlerte); break;
                 case "0":
                     continuerLeQuart = false;
                     Console.WriteLine("Fin du quart."); break;
@@ -31,15 +52,15 @@ class Program
         }
     }
 
-    static void FaireRonde()
+    static void FaireRonde(Navire navire, RondeurSecurite rondeur, JournalDeBord journal, CentreAlerte centreAlerte)
     {
         Console.WriteLine("(à coder)");
     }
-    static void AssurerVeille()
+    static void AssurerVeille(Navire navire, VeilleurCoupee veilleur, JournalDeBord journal, CentreAlerte centreAlerte)
     {
         Console.WriteLine("(à coder)");
     }
-    static void DeclarerIncident()
+    static void DeclarerIncident(JournalDeBord journal, CentreAlerte centreAlerte)
     {
         Console.WriteLine("(à coder)");
     }
